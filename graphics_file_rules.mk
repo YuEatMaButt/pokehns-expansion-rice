6d96aaefaf3d6141acfb6775bac8e008245513e3
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-05-08 15:24:00 -0400

    fixed title screen not appearing

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -19,6 +19,24 @@ contest_types := cool beauty cute smart tough
 $(TITLESCREENGFXDIR)/pokemon_logo.gbapal: %.gbapal: %.pal
 	$(GFX) $< $@ -num_colors 224
 
+$(TITLESCREENGFXDIR)/hns/pokemon_logo.gbapal: %.gbapal: %.pal
+	$(GFX) $< $@ -num_colors 224
+
+$(TITLESCREENGFXDIR)/emerald_version.8bpp: %.8bpp: %.png
+	$(GFX) $< $@ -mwidth 8 -mheight 4
+
+graphics/pokemon_jump/bg.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -num_tiles 63 -Wnum_tiles
+
+graphics/pokenav/region_map/map.8bpp: %.8bpp: %.png
+	$(GFX) $< $@ -num_tiles 233 -Wnum_tiles
+
+$(MISCGFXDIR)/japanese_hof.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -num_tiles 29 -Wnum_tiles
+
+$(MISCGFXDIR)/mirage_tower.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -num_tiles 73 -Wnum_tiles
+
 $(BATINTGFXDIR)/textbox.gbapal: $(BATINTGFXDIR)/textbox_0.gbapal \
                                 $(BATINTGFXDIR)/textbox_1.gbapal
 	@cat $^ >$@
