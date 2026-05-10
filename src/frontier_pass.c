ff11e68ae71aa1e4ee1ba5646c5801b0127db349
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-05-10 13:52:35 -0400

    map heads hns

--- src/frontier_pass.c
+++ src/frontier_pass.c
@@ -176,6 +176,18 @@ static void PrintAreaDescription(u8);
 static void ShowHideZoomingArea(bool8, bool8);
 static void SpriteCB_PlayerHead(struct Sprite *);
 
+#if IS_HNS
+static const u16 sMaleHead_Pal[]                 = INCBIN_U16("graphics/frontier_pass/map_heads_hns.gbapal");
+static const u16 sFemaleHead_Pal[]               = INCBIN_U16("graphics/frontier_pass/map_heads_hns.gbapal");
+static const u32 sHeads_Gfx[]                    = INCBIN_U32("graphics/frontier_pass/map_heads_hns.4bpp.smol");
+#else
+static const u16 sMaleHead_Pal[]                 = INCBIN_U16("graphics/frontier_pass/map_heads.gbapal");
+static const u16 sFemaleHead_Pal[]               = INCBIN_U16("graphics/frontier_pass/map_heads_female.gbapal");
+static const u32 sHeads_Gfx[]                    = INCBIN_U32("graphics/frontier_pass/map_heads.4bpp.smol");
+#endif
+static const u32 sMapScreen_Gfx[]                = INCBIN_U32("graphics/frontier_pass/map_screen.4bpp.smol");
+static const u32 sCursor_Gfx[]                   = INCBIN_U32("graphics/frontier_pass/cursor.4bpp.smol");
+static const u32 sMapCursor_Gfx[]                = INCBIN_U32("graphics/frontier_pass/map_cursor.4bpp.smol");
 static const u16 sMaleHead_Pal[]                 = INCGFX_U16("graphics/frontier_pass/map_heads.png", ".gbapal");
 static const u16 sFemaleHead_Pal[]               = INCGFX_U16("graphics/frontier_pass/map_heads_female.pal", ".gbapal");
 static const u32 sMapScreen_Gfx[]                = INCGFX_U32("graphics/frontier_pass/map_screen.png", ".4bpp.smol");
