f24107e956123e65830599e48692e398ddefeb88
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-05-11 13:32:27 -0400

    Fast battles and fast intros

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2176,7 +2176,7 @@ static void Cmd_waitmessage(void)
         else
         {
             u16 toWait = cmd->time;
-            if (gTestRunnerHeadless)
+            if (gTestRunnerHeadless || !gSaveBlock3Ptr->challengeSettings.fastBattle)
                 gPauseCounterBattle = toWait;
             if (++gPauseCounterBattle >= toWait)
             {
@@ -4922,7 +4922,7 @@ static void Cmd_pause(void)
     if (gBattleControllerExecFlags == 0)
     {
         u16 value = cmd->frames;
-        if (gTestRunnerHeadless)
+        if (gTestRunnerHeadless || !gSaveBlock3Ptr->challengeSettings.fastBattle)
             gPauseCounterBattle = value;
         if (++gPauseCounterBattle >= value)
         {
