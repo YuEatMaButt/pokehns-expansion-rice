a1e69c92368a55ed3c3222f7b99ccdebf122514c
Andrew Strauss <astrauss11@gmail.com>
2026-06-14 18:47:10 -0400

    Add save version flag
    Signed-off-by: Andrew Strauss <astrauss11@gmail.com>

--- include/global.h
+++ include/global.h
@@ -1197,6 +1197,7 @@ struct MomSavingsData
 
 struct SaveBlock1
 {
+             u16 saveVersion;
     /*0x00*/ struct Coords16 pos;
     /*0x04*/ struct WarpData location;
     /*0x0C*/ struct WarpData continueGameWarp;
