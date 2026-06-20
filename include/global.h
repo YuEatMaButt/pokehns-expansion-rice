78c4216d25fc06014ad73e89d96e58163619ca9f
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-06-20 08:59:10 -0400

    Revert "Ported key item wheel from ultra-eclipse (#10)"
    This reverts commit b97af301faa43110e3e95199d670261c3586be7e.

--- include/global.h
+++ include/global.h
@@ -1216,7 +1216,7 @@ struct SaveBlock1
     /*0x238*/ struct Pokemon playerParty[PARTY_SIZE];
     /*0x490*/ u32 money;
     /*0x494*/ u16 coins;
-    /*0x496*/ u16 registeredItems[MAX_REGISTERED_ITEMS]; // registered for use with SELECT button
+    /*0x496*/ u16 registeredItem; // registered for use with SELECT button
     /*0x498*/ struct ItemSlot pcItems[PC_ITEMS_COUNT];
     /*0x560 -> 0x848 is bag storage*/
     /*0x560*/ struct Bag bag;
