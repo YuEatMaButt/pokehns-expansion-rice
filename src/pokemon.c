c7b0cd3d69d2aa17d9b418926eb36eb505bb4195
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-08-06 10:43:41 -0400

    Wild item rates

--- src/pokemon.c
+++ src/pokemon.c
@@ -8345,7 +8345,7 @@ void SetWildMonHeldItem(void)
         u16 i;
         bool32 itemHeldBoost = CanFirstMonBoostHeldItemRarity();
         u16 chanceNoItem = itemHeldBoost ? 20 : 45;
-        u16 chanceNotRare = itemHeldBoost ? 80 : 95;
+        u16 chanceNotRare = itemHeldBoost ? 80 : 90;
 
         for (i = 0; i < count; i++)
         {
