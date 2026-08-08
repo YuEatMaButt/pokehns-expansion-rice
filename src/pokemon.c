dc1b64e1d519c615c237bb6c4d52d57acb18e96b
Andrew Strauss <astrauss11@gmail.com>
2026-08-08 19:31:27 -0400

    Separate invalid/disabled species messages
    Signed-off-by: Andrew Strauss <astrauss11@gmail.com>

--- src/pokemon.c
+++ src/pokemon.c
@@ -9341,7 +9341,12 @@ bool32 TryBoxMonFormChange(struct BoxPokemon *boxMon, enum FormChanges method)
 
 u16 SanitizeSpeciesId(u16 species)
 {
-    assertf(species <= NUM_SPECIES && (species == SPECIES_NONE || IsSpeciesEnabled(species)), "invalid species: %d", species)
+    assertf(species <= NUM_SPECIES, "invalid species: %d", species)
+    {
+        return SPECIES_NONE;
+    }
+
+    assertf(species == SPECIES_NONE || IsSpeciesEnabled(species), "disabled species: %d", species)
     {
         return SPECIES_NONE;
     }
