fc8821aeef1bbc8a9d0050837e4cb51b2490f562
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-08-08 10:30:53 -0400

    Fix violet door sound

--- src/field_door.c
+++ src/field_door.c
@@ -569,7 +569,7 @@ static const struct DoorGraphics sDoorAnimGraphicsTable[] =
     {METATILE_Fuchsia_Door_Hns,                                &gTileset_Fuchsia_Hns, DOOR_SOUND_NORMAL,  1, sDoorAnimTiles_HnsFuchsia, sDoorAnimPalettes_FuchsiaHns},
     {METATILE_SafariZoneJohto_Door_Hns,                        &gTileset_SafariZoneJohto_Hns, DOOR_SOUND_SLIDING, 1, sDoorAnimTiles_HnsSafariZone, sDoorAnimPalettes_JohtoSafariZone},
     {METATILE_CherrygroveCity_Door_Red_Hns,                    &gTileset_CherrygroveCity_Hns, DOOR_SOUND_NORMAL,  1, sDoorAnimTiles_NewBarkTownRed, sDoorAnimPalettes_CherrygroveRed},
-    {METATILE_VioletCity_Dojo_Door_Hns,                        &gTileset_VioletCity_Hns, DOOR_SOUND_SLIDING, 1, sDoorAnimTiles_DojoDoor, sDoorAnimPalettes_VioletDojo},
+    {METATILE_VioletCity_Dojo_Door_Hns,                        &gTileset_VioletCity_Hns, DOOR_SOUND_NORMAL, 1, sDoorAnimTiles_DojoDoor, sDoorAnimPalettes_VioletDojo},
     {METATILE_EcruteakCity_Door_Hns,                           &gTileset_Ecruteak_City_Hns, DOOR_SOUND_NORMAL,  1, sDoorAnimTiles_DojoDoorEcruteak, sDoorAnimPalettes_EcruteakDojo},
     {METATILE_MtSilverNewSinjoh_Door_Hns,                      &gTileset_MtSilver_NewSinjoh_Hns, DOOR_SOUND_NORMAL,  1, sDoorAnimTiles_MtSilverNewSinjoh, sDoorAnimPalettes_MtSilverNewSinjoh},
     {METATILE_OlivineCity_Door_Hns,                            &gTileset_OlivineCity_Hns, DOOR_SOUND_NORMAL,  1, sDoorAnimTiles_Olivine, sDoorAnimPalettes_Olivine},
