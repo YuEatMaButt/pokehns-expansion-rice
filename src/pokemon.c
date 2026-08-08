72f74ff11cc7ac42d911b1a32a21e84fc3663e8a
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-08-08 10:29:15 -0400

    Add rival battle music

--- src/pokemon.c
+++ src/pokemon.c
@@ -8053,6 +8053,8 @@ u16 GetBattleBGM(void)
             if (!StringCompare(GetTrainerNameFromId(TRAINER_BATTLE_PARAM.opponentA), gText_BattleWallyName))
                 return MUS_VS_TRAINER;
             return MUS_VS_RIVAL;
+        case TRAINER_CLASS_RIVAL_HNS:
+            return MUS_HG_VS_RIVAL;
         case TRAINER_CLASS_ELITE_FOUR:
             return MUS_VS_ELITE_FOUR;
         case TRAINER_CLASS_CHAMPION_FRLG:
