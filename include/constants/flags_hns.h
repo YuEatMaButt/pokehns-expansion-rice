3dbed31c83b54cc52b74f4fe165cb039523d86e0
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-08-08 14:52:12 -0400

    Fix flag collision

--- include/constants/flags_hns.h
+++ include/constants/flags_hns.h
@@ -673,7 +673,6 @@
 #define FLAG_MINTS_CLERK                            0x262
 #define FLAG_MINTS_ENABLED                          0x263
 #define FLAG_NO_SHINY                               0x264
-#define FLAG_ENABLE_CONDITION                        0x265
 
 #define HNS_FEATURE_COUNT                           10
 
@@ -847,7 +846,7 @@
 #define FLAG_FUCHSIA_SAFARI_NPC_TRADE_COMPLETED      0x300
 #define FLAG_PEWTER_GYM_NPC_TRADE_COMPLETED          0x301
 #define FLAG_OLIVINE_GYM_NPC_TRADE_COMPLETED         0x302
-#define FLAG_UNUSED_35                              0x303
+#define FLAG_ENABLE_CONDITION                       0x303
 #define FLAG_UNUSED_36                              0x304
 #define FLAG_UNUSED_37                              0x305
 #define FLAG_UNUSED_38                              0x306
