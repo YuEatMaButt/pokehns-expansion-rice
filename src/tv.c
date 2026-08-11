9df87193691d002053ac9738af4da1a09c408c1f
Andrew Strauss <astrauss11@gmail.com>
2026-08-11 12:58:57 -0400

    Fix various functions randomly generating invalid species
    Signed-off-by: Andrew Strauss <astrauss11@gmail.com>

--- src/tv.c
+++ src/tv.c
@@ -3074,7 +3074,7 @@ static u16 GetRandomDifferentSpeciesSeenByPlayer(u16 excludedSpecies)
     u16 species = Random() % (NUM_SPECIES - 1) + 1;
     u16 initSpecies = species;
 
-    while (GetSetPokedexFlag(SpeciesToNationalPokedexNum(species), FLAG_GET_SEEN) != TRUE || species == excludedSpecies)
+    while (IsSpeciesEnabled(species) == FALSE || GetSetPokedexFlag(SpeciesToNationalPokedexNum(species), FLAG_GET_SEEN) != TRUE || species == excludedSpecies)
     {
         if (species == SPECIES_NONE + 1)
             species = NUM_SPECIES - 1;
