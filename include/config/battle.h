PokemonHnS-Development/pokehns-expansion

Added exp on catch for pokemonhns parity

include/config/battle.h
@@ -11,7 +11,7 @@
 #define B_LIGHT_BALL_ATTACK_BOOST   GEN_LATEST // In Gen4+, Light Ball doubles the power of physical moves in addition to special moves.
 
 // Experience settings
-#define B_EXP_CATCH                 GEN_3     // In Gen6+, Pokémon get experience from catching.
+#define B_EXP_CATCH                 GEN_6     // In Gen6+, Pokémon get experience from catching.
 #define B_TRAINER_EXP_MULTIPLIER    GEN_3     // In Gen7+, trainer battles no longer give a 1.5 multiplier to EXP gain.
 #define B_SPLIT_EXP                 GEN_LATEST // In Gen6+, all participating mon get full experience.
 #define B_SCALED_EXP                GEN_3     // In Gen5 and Gen7+, experience is weighted by level difference.

YuEatMaButt/pokehns-expansion-rice

include/config/battle.h
@@ -11,7 +11,7 @@
 #define B_LIGHT_BALL_ATTACK_BOOST   GEN_LATEST // In Gen4+, Light Ball doubles the power of physical moves in addition to special moves.
 
 // Experience settings
-#define B_EXP_CATCH                 GEN_3     // In Gen6+, Pokémon get experience from catching.
+#define B_EXP_CATCH                 GEN_LATEST     // In Gen6+, Pokémon get experience from catching.
 #define B_TRAINER_EXP_MULTIPLIER    GEN_3     // In Gen7+, trainer battles no longer give a 1.5 multiplier to EXP gain.
 #define B_SPLIT_EXP                 GEN_LATEST // In Gen6+, all participating mon get full experience.
 #define B_SCALED_EXP                GEN_3     // In Gen5 and Gen7+, experience is weighted by level difference.